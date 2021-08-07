7859590ca028d4074841794f585ad6fd073d04f1
Anna Kuznetsova <annrid@ya.ru>
2021-08-07 23:39:06 +0300

    added xlsxtest

--- src/test/java/guru/qa/XlsxTest.java
+++ src/test/java/guru/qa/XlsxTest.java
@@ -1,4 +1,14 @@
 package guru.qa;
 
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static testUtils.FileUtils.readXlsxFile;
+
 public class XlsxTest {
+    @Test
+    public void XlsxFile() throws Exception {
+        String exelData = readXlsxFile("file_example_XLSX_10.xlsx", 0, 5, 5);
+        assertThat(exelData, containsString("58"));
+    }
 }
--- src/test/java/testUtils/FileUtils.java
+++ src/test/java/testUtils/FileUtils.java
@@ -4,15 +4,20 @@ import net.lingala.zip4j.core.ZipFile;
 import org.apache.commons.io.IOUtils;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.openxml4j.opc.OPCPackage;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
-
 
 public class FileUtils {
 
@@ -30,6 +35,20 @@ public class FileUtils {
         String text = stripper.getText(document);
         document.close();
         return text;
+    }
+
+    public static InputStream getInputStreamForFileName(String fileName) {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        return classLoader.getResourceAsStream(fileName);
+    }
+
+    public static String readXlsxFile (String xlsxFilePath, int index, int rowindex, int cellnum) throws Exception {
+        InputStream stream = getInputStreamForFileName(xlsxFilePath);
+        XSSFWorkbook workbook = new XSSFWorkbook(stream);
+        XSSFSheet sheet = workbook.getSheetAt(index);
+        XSSFRow row = sheet.getRow(rowindex);
+        XSSFCell cell = row.getCell(cellnum);
+        return String.valueOf(cell.getNumericCellValue());
     }
 
     public static void readZipFile (String zipFilePath, String password, String unzipFolderPath) throws Exception {
